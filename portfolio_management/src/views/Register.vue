f7ec4eae413840d2c8c627b54696c2c4034bd37c
Nigel Koh <kohyuanqi@gmail.com>
2023-10-27 19:13:07 +0800

    navbar sign in sign up base

--- portfolio_management/src/views/Register.vue
+++ portfolio_management/src/views/Register.vue
@@ -1,10 +1,60 @@
 <template>
-  <h1>Create an Account</h1>
+  <!-- <h1>Create an Account</h1>
   <p><input type="text" placeholder="username" v-model="username" /></p>
   <p><input type="text" placeholder="email" v-model="email" /></p>
   <p><input type="password" placeholder="password" v-model="password" /></p>
   <p><button @click="register">Register</button></p>
-  <p><router-link to="/login">Already have an account?</router-link></p>
+  <p><router-link to="/login">Already have an account?</router-link></p> -->
+
+  <main class="main-content mt-0">
+    <!-- min-vh-100 to scroll -->
+    <section>
+      <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg"
+        style="background-image: url('src/assets/img/curved-images/curved14.jpg');">
+        <span class="mask bg-gradient-dark opacity-6"></span>
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-5 text-center mx-auto">
+              <h1 class="text-white mb-2 mt-5">Welcome!</h1>
+              <p class="text-lead text-white">You are one step closer to a better stock portfolio!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row mt-lg-n10 mt-md-n11 mt-n10">
+          <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
+            <div class="card z-index-0">
+              <div class="card-header text-center pt-4">
+                <h5>Registration</h5>
+              </div>
+              <div class="card-body">
+                <form role="form text-left">
+                  <div class="mb-3">
+                    <input type="text" class="form-control" placeholder="Name" aria-label="Name"
+                      aria-describedby="email-addon">
+                  </div>
+                  <div class="mb-3">
+                    <input type="email" class="form-control" placeholder="Email" aria-label="Email"
+                      aria-describedby="email-addon">
+                  </div>
+                  <div class="mb-3">
+                    <input type="password" class="form-control" placeholder="Password" aria-label="Password"
+                      aria-describedby="password-addon">
+                  </div>
+                  <div class="text-center">
+                    <button type="button" class="btn bg-gradient-dark w-100 my-4 mb-2">Sign up</button>
+                  </div>
+                  <p class="text-sm mt-3 mb-0">Already have an account? 
+                    <router-link to="/login" class="text-dark font-weight-bolder">Sign In</router-link></p>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script setup>
