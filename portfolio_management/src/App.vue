c3d540f0b278313620295329066879a3679e0a37
Nigel Koh <kohyuanqi@gmail.com>
2023-10-27 19:13:07 +0800

    navbar sign in sign up base

--- portfolio_management/src/App.vue
+++ portfolio_management/src/App.vue
@@ -1,27 +1,53 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand">Portfolio Management</a>
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 justify-content-end">
-        <li class="nav-item">
-          <RouterLink to="/">Portfolio</RouterLink>
-        </li>
-        <li class="nav-item">
-          <RouterLink to="/markets">Markets</RouterLink>
-        </li>
-        <li class="nav-item">
-          <RouterLink to="/stockList">Search</RouterLink>
-        </li>
-        <li class="nav-item">
-          <RouterLink to="/stockpage">Stock Page(TEMP)</RouterLink>
-        </li>
-      </ul>
-      <form @submit.prevent class="d-flex" role="log-in">
-        <button @click="handleLogIn" v-if="!isLoggedIn">Log In</button>
-      </form>
-      <form @submit.prevent class="d-flex" role="log-out">
-        <button @click="handleLogOut" v-if="isLoggedIn">Log Out</button>
-      </form>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <link id="pagestyle" href="src/assets/css/soft-ui-dashboard.css?v=1.0.7" rel="stylesheet" />
+  <link href="src/assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="src/assets/css/nucleo-svg.css" rel="stylesheet" />
+
+  <nav
+    class="navbar navbar-expand-lg blur blur-rounded top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-10">
+    <div class="container-fluid pe-0">
+      <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3">
+        Portfolio Management
+      </a>
+      <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation"
+        aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon mt-2">
+          <span class="navbar-toggler-bar bar1"></span>
+          <span class="navbar-toggler-bar bar2"></span>
+          <span class="navbar-toggler-bar bar3"></span>
+        </span>
+      </button>
+      <div class="collapse navbar-collapse" id="navigation">
+        <ul class="navbar-nav mx-auto ms-xl-auto">
+          <li class="nav-item">
+            <a class="nav-link d-flex align-items-center me-2">
+              <i class="fa fa-chart-pie opacity-6 text-dark me-1"></i>
+              <RouterLink to="/">Portfolio</RouterLink>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link me-2">
+              <i class="fa fa-user opacity-6 text-dark me-1"></i>
+              <RouterLink to="/markets">Markets</RouterLink>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link me-2">
+              <i class="fas fa-user-circle opacity-6 text-dark me-1"></i>
+              <RouterLink to="/stockList">Search</RouterLink>
+            </a>
+          </li>
+        </ul>
+        <ul class="navbar-nav d-lg-block d-none">
+          <li class="nav-item">
+            <a class="btn btn-sm btn-round mb-0 me-1 bg-gradient-dark" @click="handleLogIn" v-if="!isLoggedIn">Sign In</a>
+          </li>
+          <li class="nav-item">
+            <a class="btn btn-sm btn-round mb-0 me-1 bg-gradient-dark" @click="handleLogOut" v-if="isLoggedIn">Sign Out</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
   <RouterView />
