2e1edc7ae638e8c7bb0afd038d4fe7c9abc68030
khhui <kuekhuiyi23@gmail.com>
2023-11-04 11:21:02 +0800

    added css and bs for stockpage and markets

--- portfolio_management/src/views/StockPage.vue
+++ portfolio_management/src/views/StockPage.vue
@@ -1,104 +1,203 @@
 <template>
-  <div class="container-fluid text-center mt-7">
-    <div class="container">
-      <!-- Display the selected stock symbol -->
-      <h1 id="stockSymbol">{{ `Stock Symbol: ${selectedSymbol} ${selectedName} (${curr_interval})` }}</h1>
-    </div>
+  <main>
+    <div class="container-fluid mt-7" style="background-image: url('src\assets\img\curved-images\curved-10.jpg')">
+      <div class="col-lg-12 mx-auto">
+        <div class="card z-index-2 blur blur-rounded shadow-lg rounded-5 mt-7">
+          <div class="card-header pb-0 pt-2">
+            <!-- Display the selected stock symbol -->
+            <h1 class="text-center responsive-h1" id="stockSymbol">{{ `Stock Symbol: ${selectedSymbol} ${selectedName} (${curr_interval})` }}</h1>
+          </div>
 
-    <div class="container" id="chart">
-      <!-- The chart will be rendered here -->
-    </div>
 
-    <div class="container-fluid">
-      <div class="row justify-content-between">
-        <div class="col-3" id="intervalOptions">
-          <button id="allButton" @click="fetchDataAndUpdateChart('All')">All Time</button>
-          <button id="1m" @click="fetchDataAndUpdateChart('Monthly')">Monthly</button>
-          <button id="1wk" @click="fetchDataAndUpdateChart('Weekly')">Weekly</button>
+          <div class="card-body p-3">
+            <div class="container-fluid">
+              <div class="row justify-content-center">
+                <div class="col-12 mx-auto" style="overflow-x:auto; max-width: 100%;" id="chart">
+                  <!-- The candlestick chart will be rendered here -->
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="container-fluid">
+            <div class="row justify-content-between">
+              <div class="col-lg-6 col-md-7 mb-3" id="interval-options">
+              <!-- <div class="col-4 col-lg-6 text-center" id="intervalOptions"> -->
+                <button class="btn bg-gradient-primary mx-1 d-sm-inline d-block" id="allButton" @click="fetchDataAndUpdateChart('All')">All Time</button>
+                <button class="btn bg-gradient-primary mx-1 d-sm-inline d-block" id="1m" @click="fetchDataAndUpdateChart('Monthly')">Monthly</button>
+                <button class="btn bg-gradient-primary mx-1 d-sm-inline d-block" id="1wk" @click="fetchDataAndUpdateChart('Weekly')">Weekly</button>
+              </div>
+
+              <div class="col-xl-2 col-lg-1 col-md-6 mb-3" style="min-width: 25vh;" id="chart-type">
+              <!-- <div class="col-3 col-lg-4 text-center" id="chart-type"> -->
+                <button class="btn bg-gradient-primary mx-1" id="candlestick"
+                  @click="chartType = 'candlestick'; fetchDataAndUpdateChart(curr_interval)">Candlestick</button>
+                <button class="btn bg-gradient-primary mx-1" id="area" @click="chartType = 'area'; fetchDataAndUpdateChart(curr_interval)">Area</button>
+              </div>
+            </div>
+          </div>
+        </div> 
+
+        <div class="card blur blur-rounded shadow rounded-5 mt-4">
+          <!-- market Sentiment -->
+          <div class="card-body p-3">
+            <div class="container text-center">
+              <div class="row">
+                <div class="col-md-6 col-sm-12">
+                  <div class="sentiment-label">
+                    <h3 class="responsive-h3">Market Sentiment: </h3>
+                    <span :class="[sentimentBool ? 'text-positive' : 'text-negative']">{{ sentiment }}</span>
+                  </div>
+                </div>
+
+                <div class="col-md-6 col-sm-12 col-12">
+                  <div id="sentimentLine" class="table-responsive">
+                    <table class="table my-2">
+                      <thead>
+                        <tr>
+                          <th class="text-uppercase text-secondary font-weight-bolder opacity-7 p-0 p-sm-2">Positive</th>
+                          <th class="text-uppercase text-secondary font-weight-bolder opacity-7 ps-sm-5 ps-2 p-sm-2 p-0">Negative</th>
+                        </tr>
+                      </thead>
+
+                      <tbody>
+                        <tr>
+                          <td class="text-center">{{ positive }}</td>
+                          <td class="text-center ps-sm-5 ps-2">{{ negative }}</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>            
+          </div>  
         </div>
-        <div class="col-3" id="chart-type">
-          <button id="candlestick"
-            @click="chartType = 'candlestick'; fetchDataAndUpdateChart(curr_interval)">Candlestick</button>
-          <button id="area" @click="chartType = 'area'; fetchDataAndUpdateChart(curr_interval)">Area</button>
+
+        <div class="card blur blur-rounded shadow rounded-5 mt-4">
+          <div class="card-body py-0">
+            <div class="row">
+              <div class="container-fluid justify-content-center" id="news">
+                <!-- only first 2 will show -->
+                <div class="row justify-content-center">
+                  <div v-for="article in newsData.articles.slice(0, 2)" :key="article.title" class="col-md-6 justify-content-center mt-4 p-0">
+                    <div class="card w-auto border m-sm-2 news" style="width:500px">
+                      <img height="550" :src="article.urlToImage" class="card-img-top img-fluid" alt="...">
+                      <div class="card-body d-flex flex-column p-md-2 p-2" style="height: 180px;">
+                        <h5 class="card-title responsive-h5">{{ article.title }}</h5>
+                        <p v-if="article.description" class="card-text text-truncate responsive-text">{{ article.description }}...</p>
+                        <a :href="article.url" target="_blank" class="btn btn-primary mb-0">Read Article</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div id="showmore" class="container-fluid justify-content-center" v-show="showButtonValue">
+                <div class="row justify-cotent-center">
+                  <div v-for=" article  in  newsData.articles.slice(2) " :key="article.title" class="col-md-6 justify-content-center mt-4 p-0">
+                    <div class="card w-auto border m-sm-2 news" style="width:500px">
+                      <img height="550" :src="article.urlToImage" class="card-img-top img-fluid" alt="...">
+                      <div class="card-body d-flex flex-column p-md-2 p-2" style="height: 200px;">
+                        <h5 class="card-title responsive-h5">{{ article.title }}</h5>
+                        <p v-if="article.description" class="card-text text-truncate responsive-text">{{ article.description.slice(0, 100) }}...
+                        </p>
+                        <a :href="article.url" target="_blank" class="btn btn-primary">Read Article</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="row mt-2 mx-3">
+              <button @click="toggleShowMore" id="showButton" class="btn bg-gradient-primary">
+                {{ showButtonValue ? 'Show Less' : 'Show More' }}
+              </button>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="card blur blur-rounded shadow rounded-5 my-4">
+          <div class="card-header text-center my-0 pb-0">
+            <h3 class="responsive-h3">Forum</h3>
+          </div>
+          <div class="card-body py-0">
+            <div class="row">
+              <div class="container form-group font-weight-bold mt-2">
+                <input class="form-control responsive-text my-3 col-lg-10 col-md-10 col-sm-10" type="text" id="name-box" v-model="nameInput" placeholder="Enter name" />
+                <textarea class="form-control responsive-text col-lg-10 col-md-10 col-sm-10" type="text" id="comment-box" v-model="commentInput" placeholder="Enter comment" />
+              </div>
+            </div>
+
+            <div class="row mx-3 mb-sm-5 m-4">
+              <button class="btn bg-gradient-primary" id="post" @click="addComment()">Comment</button>
+            </div>
+
+            <div class="row mx-3" v-for="comment in comments">
+              <div class="card border mb-sm-4 mb-3 p-md-3 p-1">
+                <div class="card-body p-sm-5 p-md-2 p-3">
+                  <h5 class="card-title responsive-h5">{{ comment.name }}</h5>
+                  <p class="card-text responsive-text">{{ comment.comment }}</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
 
-    <!-- market Sentiment -->
-    <div class="sentiment-label">Market Sentiment: 
-      <span :class="[sentimentBool ? 'text-positive' : 'text-negative']">{{ sentiment }}</span>
-    </div>
-    <div id="sentimentLine" class="table justify-content-center">
-      <table>
-        <tr>
-          <th>Positive:</th>
-          <th>Negative:</th>
-        </tr>
 
-        <tr>
-          <td>{{ positive }}</td>
-          <td>{{ negative }}</td>
+      <!-- <div class="card m-3" v-for="comment in comments">
+        <div class="card-body">
+          <h5 class="card-title">{{ comment.name }}</h5>
+          <p class="card-text">{{ comment.comment }}</p>
+        </div>
+      </div> -->
 
 
-        </tr>
-      </table>
-    </div>
-
-
-
-    <!-- news section-->
-    <div class="container">
-      <div class="container-fluid justify-content-center" id="news">
-        <!-- only first 2 will show -->
-        <div class="row justify-content-center">
-          <div v-for=" article  in  newsData.articles.slice(0, 2) " :key="article.title" class="col-xxl-6">
-            <div class="card m-2 news" style="width:500px">
-              <img height="300" :src="article.urlToImage" class="card-img-top" alt="...">
-              <div class="card-body d-flex flex-column" style="height: 200px;">
-                <h5 class="card-title">{{ article.title }}</h5>
-                <p v-if="article.description" class="card-text  text-truncate">{{ article.description }}...</p>
-                <a :href="article.url" target="_blank" class="btn btn-primary mt-auto">Read Article</a>
+      <!-- news section-->
+      <!-- <div class="container">
+        <div class="container-fluid justify-content-center" id="news"> -->
+          <!-- only first 2 will show -->
+          <!-- <div class="row justify-content-center"> -->
+            <!-- <div v-for=" article  in  newsData.articles.slice(0, 2) " :key="article.title" class="col-xxl-6">
+              <div class="card m-2 news" style="width:500px">
+                <img height="300" :src="article.urlToImage" class="card-img-top" alt="...">
+                <div class="card-body d-flex flex-column" style="height: 200px;">
+                  <h5 class="card-title">{{ article.title }}</h5>
+                  <p v-if="article.description" class="card-text  text-truncate">{{ article.description }}...</p>
+                  <a :href="article.url" target="_blank" class="btn btn-primary mt-auto">Read Article</a>
+                </div>
               </div>
             </div>
           </div>
         </div>
 
-      </div>
-      <div id="showmore" class="container-fluid justify-content-center" v-show="showButtonValue">
-        <div class="row justify-cotent-center">
-          <div v-for=" article  in  newsData.articles.slice(2) " :key="article.title"
-            class="col-xxl-6 justify-content-center">
-            <div class="card m-2 news" style="width:500px">
-              <img height="300" :src="article.urlToImage" class="card-img-top" alt="...">
-              <div class="card-body d-flex flex-column" style="height: 200px;">
-                <h5 class="card-title">{{ article.title }}</h5>
-                <p v-if="article.description" class="card-text text-truncate">{{ article.description.slice(0, 100) }}...
-                </p>
-                <a :href="article.url" target="_blank" class="btn btn-primary mt-auto">Read Article</a>
+        <div id="showmore" class="container-fluid justify-content-center" v-show="showButtonValue">
+          <div class="row justify-cotent-center">
+            <div v-for=" article  in  newsData.articles.slice(2) " :key="article.title"
+              class="col-xxl-6 justify-content-center">
+              <div class="card m-2 news" style="width:500px">
+                <img height="300" :src="article.urlToImage" class="card-img-top" alt="...">
+                <div class="card-body d-flex flex-column" style="height: 200px;">
+                  <h5 class="card-title">{{ article.title }}</h5>
+                  <p v-if="article.description" class="card-text text-truncate">{{ article.description.slice(0, 100) }}...
+                  </p>
+                  <a :href="article.url" target="_blank" class="btn btn-primary mt-auto">Read Article</a>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <button @click="toggleShowMore" id="showButton" class="btn btn-primary" style="margin: 10px">
-      {{ showButtonValue ? 'Show Less' : 'Show More' }}
-    </button>
+      <button @click="toggleShowMore" id="showButton" class="btn btn-primary" style="margin: 10px">
+        {{ showButtonValue ? 'Show Less' : 'Show More' }}
+      </button> -->
 
-    <div class="container" id="comments-form">
-      <input type="text" id="name-box" v-model="nameInput" placeholder="Enter name" />
-      <input type="text" id="comment-box" v-model="commentInput" placeholder="Enter comment" />
-      <button id="post" @click="addComment()">Comment</button>
-      <div id="comments-section">
-        <div class="card m-3" v-for=" comment  in  comments ">
-          <div class="card-body">
-            <h5 class="card-title">{{ comment.name }}</h5>
-            <p class="card-text">{{ comment.comment }}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  </main>
 </template>
 
 <script setup>
@@ -203,30 +302,33 @@ const renderChart = (chartData) => {
         type: chartType,
         name: tickerSymbol.value,
         height: 400,
-        width: 1200,
+        width: 1700
       },
       series: [
         {
-          data: chartData,
-        },
+          data: chartData
+        }
       ],
       xaxis: {
-        type: 'datetime',
+        type: 'datetime'
       },
       yaxis: {
         tooltip: {
-          enabled: true,
-        },
+          enabled: true
+        }
       },
       dataLabels: {
-        enabled: false,
-      },
-    };
+        enabled: false
+      }
+    }
 
     // Render the chart
     chart = new ApexCharts(document.querySelector('#chart'), chartOptions)
     chart.render()
     console.log('Chart Rendered')
+
+    // Handle window resize events
+    window.addEventListener('resize', resizeChart);
   } else {
     if (chartType == 'area') {
       chart.updateSeries([
@@ -247,6 +349,28 @@ const renderChart = (chartData) => {
     console.log('Chart Updated')
   }
 }
+
+const resizeChart = () => {
+  const screenWidth = window.innerWidth;
+
+  if (chart) {
+    if (screenWidth < 576) {
+      chart.updateOptions({
+        chart: {
+          height: 400,
+          width: 450, 
+        },
+      });
+    } else {
+      chart.updateOptions({
+        chart: {
+          height: 400,
+          width: document.querySelector('#candlestick-chart').clientWidth,
+        },
+      });
+    }
+  }
+};
 
 async function fetchData(interval, symbol) {
   let historical_data = {
@@ -449,8 +573,10 @@ const addComment = () => {
 </script>
 
 <style>
+@import '../assets/css/soft-ui-dashboard.css';
+
 .sentiment-label {
-  margin-bottom: 10px;
+  padding: 15px 0px;
   font-weight: bold;
   font-size: 30px;
 }
@@ -470,13 +596,109 @@ const addComment = () => {
 }
 
 
-
 .text-positive {
   color: green;
 }
 
 .text-negative {
   color: red;
+}
+
+
+@media (min-width: 0px) {
+  .responsive-text {
+    font-size: 0.7rem;
+  }
+
+  
+  .responsive-h1 {
+    font-size: 24px;
+  }
+
+  .responsive-h3 {
+    font-size: 22px;
+  }
+
+  .responsive-h5 {
+    font-size: 0.9rem;
+    font-weight: bold;
+  }
+}
+
+@media (min-width: 576px) {
+  .responsive-text {
+    font-size: 0.6rem;
+  }
+  
+  .responsive-h1 {
+    font-size: 29px;
+  }
+
+  .responsive-h3 {
+    font-size: 23px;
+  }
+
+  .responsive-h5 {
+    font-size: 17px;
+    font-weight: bold;
+  }
+}
+
+@media (min-width: 768px) {
+  .responsive-text {
+    font-size: 0.8rem;
+  }
+
+  .responsive-h1 {
+    font-size: 42px;
+  }
+
+  .responsive-h3 {
+    font-size: 24px;
+  }
+
+  .responsive-h5 {
+    font-size: 16px;
+    font-weight: bold;
+  }
+}
+
+@media (min-width: 992px) {
+  .responsive-text {
+    font-size: 0.8rem;
+  }
+
+  .responsive-h1 {
+    font-size: 50px;
+  }
+
+  .responsive-h3 {
+    font-size: 30px;
+  }
+
+  .responsive-h5 {
+    font-size: 25px;
+    font-weight: bold;
+  }
+}
+
+@media (min-width: 1200px) {
+  .responsive-text {
+    font-size: 1rem;
+  }
+
+  .responsive-h1 {
+    font-size: 50px;
+  }
+
+  .responsive-h3 {
+    font-size: 30px;
+  }
+
+  .responsive-h5 {
+    font-size: 25px;
+    font-weight: bold;
+  }
 }
 </style>
 
